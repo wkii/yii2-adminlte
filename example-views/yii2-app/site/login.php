63d9211cc065892776ff7ea0fd2b78f532e2a225
wkii <digihero@gmail.com>
2016-02-25 11:10:03 +0800

    update login app name

--- example-views/yii2-app/site/login.php
+++ example-views/yii2-app/site/login.php
@@ -21,7 +21,7 @@ $fieldOptions2 = [
 
 <div class="login-box">
     <div class="login-logo">
-        <a href="#"><b>Admin</b>LTE</a>
+        <?= Yii::$app->name ? Yii::$app->name : '<b>Admin</b>LTE' ?>
     </div>
     <!-- /.login-logo -->
     <div class="login-box-body">
